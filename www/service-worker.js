b1821dbe465b1fd8c412b1e1868b35772f5c0aa4
Martin Skočík <mskocik@gmail.com>
2021-03-09 14:40:41 +0100

    sw-log

--- www/service-worker.js
+++ www/service-worker.js
@@ -29,7 +29,7 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', function(event) {
-  // console.log('[Service Worker] Fetch', event.request.url);
+  console.log('[Service Worker] Fetch', event.request.url);
   if (event.request.mode === 'navigate') {
     event.respondWith((async () => {
       try {
